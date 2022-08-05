9ad8b654052bfdbeef2e322f5c8fe3f37484115c
Rupinder Singh <rupinder.singh@nagarro.com>
2022-08-05 17:24:26 -0400

    added service

--- src/main/java/com/nagarro/nagp/controller/HelloWorldController.java
+++ src/main/java/com/nagarro/nagp/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.nagarro.nagp.controller;
 
 
+import com.nagarro.nagp.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
+
+    @Autowired
+    HelloWorldService service;
 
     @GetMapping(produces = "application/json")
     public ResponseEntity<String> hello() {
---
+++ src/main/java/com/nagarro/nagp/service/HelloWorldService.java
@@ -0,0 +1,7 @@
+package com.nagarro.nagp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+}
