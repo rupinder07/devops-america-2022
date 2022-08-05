1ef9cfc1051925196c75d4284d986388f7428af3
Rupinder Singh <rupinder.singh@nagarro.com>
2022-08-04 20:52:39 -0400

    First Commit

---
+++ src/test/java/com/nagp/devopssample/DevOpsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagp.devopssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
